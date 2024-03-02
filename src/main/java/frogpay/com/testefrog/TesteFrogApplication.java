07f5c2d7a2fecaa6bdbb6559b4b43065a24e9df6
gabriel_abrahao <gabriel.abrahao95@gmail.com>
2024-03-02 20:54:45 -0300

    first commit

---
+++ src/main/java/frogpay/com/testefrog/TesteFrogApplication.java
@@ -0,0 +1,13 @@
+package frogpay.com.testefrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFrogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFrogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/frogpay/com/testefrog/TesteFrogApplicationTests.java
@@ -0,0 +1,13 @@
+package frogpay.com.testefrog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteFrogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
