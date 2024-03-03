33669e0a9f7d629c8859e952c98b383e1351d59d
gabriel_abrahao <gabriel.abrahao95@gmail.com>
2024-03-03 14:44:34 -0300

    Adicionado dependencias de flyway e lombook e tbm configurado o flyway e e feito script de estrutura inicial

---
+++ src/main/resources/db/migration/V1__estrutura_inicial.sql
@@ -0,0 +1,64 @@
+CREATE TABLE IF NOT EXISTS public.tb_pessoa
+(
+    id uuid,
+    nome character varying(50),
+    cpf character varying(50),
+    data_nascimento date,
+    ativo boolean,
+    data_criacao timestamp with time zone,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.tb_dados_bancarios
+(
+    id_pessoa uuid,
+    codigo_banco character varying(20),
+    agencia character varying(20),
+    conta character varying(20),
+    digito_conta character varying
+);
+
+CREATE TABLE IF NOT EXISTS public.tb_loja
+(
+    id uuid,
+    id_pessoa uuid,
+    nome_fantasia character varying(500),
+    razao_social character varying(500),
+    cnpj character varying(50),
+    data_abertura date,
+    PRIMARY KEY (id_pessoa)
+);
+
+CREATE TABLE IF NOT EXISTS public.endereco
+(
+    id_pessoa uuid,
+    uf_estado character varying(2),
+    cidade character varying(500),
+    bairro character varying(500),
+    logradouro character varying(500),
+    numero character varying(20),
+    complemento character varying(500)
+);
+
+ALTER TABLE IF EXISTS public.tb_dados_bancarios
+    ADD CONSTRAINT id_pessoa FOREIGN KEY (id_pessoa)
+    REFERENCES public.tb_pessoa (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.tb_loja
+    ADD CONSTRAINT id_pessoa FOREIGN KEY (id_pessoa)
+    REFERENCES public.tb_pessoa (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.endereco
+    ADD CONSTRAINT id_pessoa FOREIGN KEY (id_pessoa)
+    REFERENCES public.tb_pessoa (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
